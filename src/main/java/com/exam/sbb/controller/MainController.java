37d4060b2f102f5d7365d1c6c74e8a78079d7d92
jijae92 <jijae92@gmail.com>
2023-07-25 15:11:17 +0900

    객체 조립시에 pathvariable도 사용 가능

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -213,7 +213,7 @@ public class MainController {
         return p;
     }
 
-    @GetMapping("/addPerson")
+    @GetMapping("/addPerson/{id}")
     @ResponseBody
     public Person addPerson(Person p) {
         return p;
