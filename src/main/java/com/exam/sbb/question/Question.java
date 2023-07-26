d4540303e866efa226c8f147b20aab32df938860
jijae92 <jijae92@gmail.com>
2023-07-26 12:48:05 +0900

    리포지터리 추가 후 테스트케이스

--- src/main/java/com/exam/sbb/question/Question.java
+++ src/main/java/com/exam/sbb/question/Question.java
@@ -2,10 +2,12 @@ package com.exam.sbb.question;
 
 
 
+import com.exam.sbb.answer.Answer;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -18,4 +20,6 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime createDate;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
