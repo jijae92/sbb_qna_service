df0540c98f54d8bc9e3339554c408e5d7e2a003e
jijae92 <jijae92@gmail.com>
2023-08-09 16:08:54 +0900

    인터페이스 상속을 사용해서 중복 제거

---
+++ src/main/java/com/exam/sbb/base/RepositoryUtil.java
@@ -0,0 +1,17 @@
+package com.exam.sbb.base;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+
+public interface RepositoryUtil {
+    @Modifying
+    @Transactional
+    @Query(value = "SET FOREIGN_KEY_CHECKS = 0", nativeQuery = true)
+    void disableForeignKeyChecks();
+    @Modifying
+    @Transactional
+    @Query(value = "SET FOREIGN_KEY_CHECKS = 1", nativeQuery = true)
+    void enableForeignKeyChecks();
+}
