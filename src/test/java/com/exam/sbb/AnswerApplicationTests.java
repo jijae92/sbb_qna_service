9d93ecc968e038a0a4a6780ad1da9e4e766fe7de
jijae92 <jijae92@gmail.com>
2023-08-09 14:19:04 +0900

    저장시마다 데이터가 쌓이지 않도록

--- src/test/java/com/exam/sbb/AnswerApplicationTests.java
+++ src/test/java/com/exam/sbb/AnswerApplicationTests.java
@@ -4,12 +4,13 @@ import com.exam.sbb.answer.Answer;
 import com.exam.sbb.answer.AnswerRepository;
 import com.exam.sbb.question.Question;
 import com.exam.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+
 
 
 
@@ -19,11 +20,28 @@ public class AnswerApplicationTests {
     private QuestionRepository questionRepository;
     @Autowired
     private AnswerRepository answerRepository;
+    private int lastSampleDataId;
+
+    @BeforeEach
+    void beforeEach(){
+        clearData();
+        createSampleData();
+    }
+
+    private void clearData() {
+        questionRepository.disableForeignKeyChecks();
+        answerRepository.truncate();
+        questionRepository.enableForeignKeyChecks();
+    }
+    private void createSampleData() {
+        
+
+    }
 
     @Test
     void 저장() {
         Question q = this.questionRepository.findById(2).get();
-        
+
         Answer a = new Answer();
         a.setContent("네 자동으로 생성됩니다.");
         a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
