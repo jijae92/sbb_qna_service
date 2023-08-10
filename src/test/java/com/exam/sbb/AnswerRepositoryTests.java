71302e0c0e7797cf6ecec582852541798c4a2b61
jijae92 <jijae92@gmail.com>
2023-08-10 14:47:36 +0900

    answer 조회

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -10,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -37,6 +40,20 @@ public class AnswerRepositoryTests {
     private void createSampleData() {
         QuestionRepositoryTests.createSampleData(questionRepository);
 
+        Question q = this.questionRepository.findById(1).get();
+
+        Answer a1 = new Answer();
+        a1.setContent("sbb는 질문답변 게시판입니다.");
+        a1.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a1.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("sbb에서는 주로 스프링 관련 내용을 다룹니다.");
+        a2.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a2.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a2);
+
     }
 
     @Test
@@ -49,4 +66,11 @@ public class AnswerRepositoryTests {
         a.setCreateDate(LocalDateTime.now());
         this.answerRepository.save(a);
     }
+
+    @Test
+    void 조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
+    }
+
 }
