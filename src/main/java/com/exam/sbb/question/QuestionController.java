5ed2d6777cacbf10d118d8d344a2a4cf5ebdc7cd
jijae92 <jijae92@gmail.com>
2023-08-23 16:57:00 +0900

    AnswerController 도입

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -5,22 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-
+@RequestMapping(value = "/question")
 @Controller
 public class QuestionController {
     @Autowired
     private QuestionService questionService;
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     //@ResponseBody
     public String list(Model model){
         List<Question> questionList =  questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = questionService.getQuestion(id);
 
